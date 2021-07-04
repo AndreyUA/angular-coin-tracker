ed5f3b4b7845d76e4e6a01f46a15f4ca204b723d
Andrey Zhygarev <infernalsteel666@gmail.com>
2021-07-04 18:45:27 +0300

    typing feature

--- src/app/auth-page/register-page/register-page.component.ts
+++ src/app/auth-page/register-page/register-page.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.scss'],
 })
-export class RegisterPageComponent {
+export class RegisterPageComponent implements AfterViewInit {
   user = {
     familyName: '',
     email: '',
@@ -15,5 +15,35 @@ export class RegisterPageComponent {
   onSubmit() {
     console.log('submit!');
     console.log(this.user);
+  }
+
+  // Typing features
+  @ViewChild('wellcome') wellcomeRef!: ElementRef;
+  @ViewChild('familyNameFocus') familyNameFocusRef!: ElementRef;
+
+  wellcomeText: string = 'Wellcome to Coin Tracker!';
+  typingText!: string;
+  counterTyping: number = 0;
+
+  typing(): void {
+    this.counterTyping++;
+    if (this.counterTyping === this.wellcomeText.length + 1) {
+      this.wellcomeRef.nativeElement.innerHTML = this.typingText;
+
+      this.familyNameFocusRef.nativeElement.focus();
+      return;
+    }
+
+    this.typingText = this.wellcomeText.slice(0, this.counterTyping);
+
+    this.wellcomeRef.nativeElement.innerHTML = this.typingText + '|';
+
+    setTimeout(() => {
+      this.typing();
+    }, 100);
+  }
+
+  ngAfterViewInit() {
+    this.typing();
   }
 }
