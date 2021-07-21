4e1f58701f492da548b0a877ab5d208523964d6c
Andrey Zhygarev <infernalsteel666@gmail.com>
2021-07-21 21:38:39 +0300

    basic styling of table page

--- src/app/table-page/table-page.component.ts
+++ src/app/table-page/table-page.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 
+const MOCK_DATA = [
+  {
+    person: 'Папа',
+    money: 600,
+    date: new Date(),
+  },
+  {
+    person: 'Мама',
+    money: 120,
+    date: new Date(),
+  },
+  {
+    person: 'Мама',
+    money: 200,
+    date: new Date(),
+  },
+  {
+    person: 'Мама',
+    money: 400,
+    date: new Date(),
+  },
+  {
+    person: 'Мама',
+    money: 300,
+    date: new Date(),
+  },
+  {
+    person: 'Мама',
+    money: 535,
+    date: new Date(),
+  },
+];
+
+interface ITransaction {
+  person: string;
+  money: number;
+  date: Date;
+}
+
 @Component({
   selector: 'app-table-page',
   templateUrl: './table-page.component.html',
-  styleUrls: ['./table-page.component.scss']
+  styleUrls: ['./table-page.component.scss'],
 })
 export class TablePageComponent implements OnInit {
+  total: number = 0;
 
-  constructor() { }
+  spend: number = 0;
 
-  ngOnInit(): void {
+  left: number = 0;
+
+  transactions: Array<ITransaction> | null = null;
+
+  ngOnInit() {
+    this.total = 10000;
+
+    this.transactions = MOCK_DATA;
+
+    if (this.transactions) {
+      let sum: number = 0;
+
+      this.transactions.forEach((transaction) => {
+        sum += transaction.money;
+      });
+
+      this.spend = sum;
+    }
+
+    this.left = this.total - this.spend;
   }
-
 }
