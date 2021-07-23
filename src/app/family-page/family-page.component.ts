0f174db27ea851553551c3a6be7924cbef5b1c69
Andrey Zhygarev <zhygarev.andrey.dev@gmail.com>
2021-07-23 16:14:58 +0300

    basic functionality of family page

--- src/app/family-page/family-page.component.ts
+++ src/app/family-page/family-page.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
 
+const MOCK_FAMILY1 = {
+  familyName: 'Andrusha, Olusha, Kirusha TEST1',
+  persons: [],
+};
+
+const MOCK_FAMILY2 = {
+  familyName: 'Andrusha, Olusha, Kirusha TEST2',
+  persons: [
+    {
+      name: 'Папа',
+    },
+    {
+      name: 'Мама',
+    },
+  ],
+};
+
+interface IPerson {
+  name: string;
+}
+interface IFamily {
+  familyName: string;
+  persons?: Array<IPerson>;
+}
 @Component({
   selector: 'app-family-page',
   templateUrl: './family-page.component.html',
-  styleUrls: ['./family-page.component.scss']
+  styleUrls: ['./family-page.component.scss'],
 })
 export class FamilyPageComponent implements OnInit {
+  family!: IFamily;
 
-  constructor() { }
+  addPersonForm!: FormGroup;
 
-  ngOnInit(): void {
+  changePersonForm!: FormGroup;
+
+  constructor() {}
+
+  onSubmitChangePersonHandler() {
+    localStorage.setItem('person', this.changePersonForm.value.changePerson);
+    console.log(this.changePersonForm.value.changePerson);
   }
 
+  onSubmitAddPersonHandler() {
+    this.family.persons?.push({ name: this.addPersonForm.value.addPerson });
+    this.addPersonForm.reset();
+  }
+
+  ngOnInit(): void {
+    this.family = MOCK_FAMILY2;
+
+    this.addPersonForm = new FormGroup({
+      addPerson: new FormControl(null, Validators.required),
+    });
+
+    if (localStorage.getItem('person')) {
+      this.changePersonForm = new FormGroup({
+        changePerson: new FormControl(localStorage.getItem('person')),
+      });
+    } else {
+      this.changePersonForm = new FormGroup({
+        changePerson: new FormControl('default'),
+      });
+    }
+  }
 }
