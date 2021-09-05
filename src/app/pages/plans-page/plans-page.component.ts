f17675119d2a2776117eadbea62428e2630c6225
Andrey Zhygarev <infernalsteel666@gmail.com>
2021-09-05 11:37:57 +0300

    plans page basic styled

--- src/app/pages/plans-page/plans-page.component.ts
+++ src/app/pages/plans-page/plans-page.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+interface ITodo {
+  content: string;
+  id?: string;
+  isFinished?: boolean;
+  isRemoved?: boolean;
+}
+
+const MOCK_TODO = [
+  {
+    content: 'Финики',
+    id: '3123234534535423421',
+    isFinished: false,
+    isRemoved: false,
+  },
+  {
+    content: 'Пюре Кирюне',
+    id: '3123234534535423421',
+    isFinished: false,
+    isRemoved: false,
+  },
+  {
+    content: 'Йогурт',
+    id: '3123234534535423421',
+    isFinished: false,
+    isRemoved: false,
+  },
+  {
+    content: 'Рис, гречка, булгур',
+    id: '3123234534535423421',
+    isFinished: false,
+    isRemoved: false,
+  },
+  {
+    content: 'Шоколадка',
+    id: '3123234534535423421',
+    isFinished: false,
+    isRemoved: false,
+  },
+];
 
 @Component({
   selector: 'app-plans-page',
   templateUrl: './plans-page.component.html',
-  styleUrls: ['./plans-page.component.scss']
+  styleUrls: ['./plans-page.component.scss'],
 })
 export class PlansPageComponent implements OnInit {
+  @ViewChild('todoInput', { static: false }) todoInput!: ElementRef;
 
-  constructor() { }
+  todos: Array<ITodo> = MOCK_TODO;
 
-  ngOnInit(): void {
+  todoForm!: FormGroup;
+
+  constructor() {}
+
+  onSubmit() {
+    console.log(this.todoForm.value.todo);
+
+    this.todos.unshift({ content: this.todoForm.value.todo })
+
+    this.todoInput.nativeElement.blur();
+    this.todoForm.reset();
   }
 
+  ngOnInit(): void {
+    this.todoForm = new FormGroup({
+      todo: new FormControl(null, Validators.required)
+    })
+  }
 }
