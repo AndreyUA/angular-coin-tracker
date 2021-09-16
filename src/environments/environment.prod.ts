b5ce520a78c8445ea3e7d8c0b28140fd6fdc4f69
Andrey Zhygarev <infernalsteel666@gmail.com>
2021-09-16 22:03:23 +0300

    added production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5000',
-  // TODO: add URL for production
-  // apiUrl: 'http://localhost:5000',
+  apiUrl: 'https://coin-tracker-server.herokuapp.com',
 };
