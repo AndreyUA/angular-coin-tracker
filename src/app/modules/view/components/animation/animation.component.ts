cc5d3069e3107f4933a6e23d5e33a65edb68085b
Andrey Zhygarev <infernalsteel666@gmail.com>
2021-12-19 16:10:45 +0200

    added right and left animation

--- src/app/modules/view/components/animation/animation.component.ts
+++ src/app/modules/view/components/animation/animation.component.ts
@@ -1,11 +1,17 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, AfterViewInit, HostListener } from '@angular/core';
 import {
   trigger,
   state,
   style,
   animate,
   transition,
+  AnimationEvent,
 } from '@angular/animations';
+
+enum PositionsType {
+  LEFT = 'left',
+  RIGHT = 'right',
+}
 
 @Component({
   selector: 'app-animation',
@@ -28,36 +34,72 @@ import {
       transition('click => unclick', [animate('0.2s ease-in-out')]),
       transition('unclick => click', [animate('0.2s ease-in-out')]),
     ]),
+    trigger('moveRightAndLeft', [
+      state(PositionsType.RIGHT, style({ marginLeft: '20px' })),
+      state(PositionsType.LEFT, style({ marginRight: '40px' })),
+      transition(`${PositionsType.RIGHT} => ${PositionsType.LEFT}`, [
+        animate('0.6s ease-in-out'),
+      ]),
+      transition(`${PositionsType.LEFT} => ${PositionsType.RIGHT}`, [
+        animate('0.6s ease-in-out'),
+      ]),
+    ]),
   ],
 })
-export class AnimationComponent {
+export class AnimationComponent implements AfterViewInit {
   isClicked: boolean = false;
 
-  private turnClickOff() {
+  position: string = PositionsType.LEFT;
+
+  private turnPositionToRight(): void {
+    this.position = PositionsType.RIGHT;
+  }
+
+  private turnPositionToLeft(): void {
+    this.position = PositionsType.LEFT;
+  }
+
+  private turnClickOff(): void {
     this.isClicked = false;
   }
 
-  private turnClickOn() {
+  private turnClickOn(): void {
     this.isClicked = true;
   }
 
   @HostListener('keydown', ['$event'])
-  clickHandler(event: MouseEvent) {
+  clickHandler(event: MouseEvent): void {
     if (event.type === 'mousedown') this.turnClickOn();
   }
 
   @HostListener('keyup', ['$event'])
-  unclickHandler(event: MouseEvent) {
+  unclickHandler(event: MouseEvent): void {
     if (event.type === 'mouseup') this.turnClickOff();
   }
 
   @HostListener('touchstart', ['$event'])
   @HostListener('touchend', ['$event'])
-  handleTouch(event: Event) {
+  handleTouch(event: Event): void {
     if (event.type === 'touchstart') this.turnClickOn();
 
     if (event.type === 'touchend') this.turnClickOff();
   }
 
+  onAnimationEnd(event: AnimationEvent): void {
+    this.turnPositionToLeft();
+
+    if (event.toState === PositionsType.LEFT) {
+      setTimeout(() => {
+        this.turnPositionToRight();
+      }, 0);
+    }
+  }
+
   constructor() {}
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.turnPositionToRight();
+    }, 0);
+  }
 }
